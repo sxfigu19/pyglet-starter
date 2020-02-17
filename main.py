c534cc006217b58146af4f7a2e2cf0c603da888c
Savannah <60404236+sxfigu19@users.noreply.github.com>
2020-02-17 14:31:17 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,17 +4,18 @@ win = pyglet.window.Window()
 
 label = pyglet.text.Label('Hello, Player!', font_size=22, x = 250, y = 450)
 labeltwo = pyglet.text.Label('Hope you are ready', font_size=17, x = 235, y = 420)
-labelthree = pyglet.text.Label('Enjoy the game!', font_size=12, x = 275, y = 395)
+labelthree = pyglet.text.Label('Click the arrow keys to move your person', font_size=12, x = 190, y = 395)
+
+
+cave= pyglet.image.load('assets/forest-assets/cave.png')
+spr1 = pyglet.sprite.Sprite(cave, x = 80, y = 0)
+spr1.scale = 10
 
 # Create a sprite so the game can keep track of coordinates
 img= pyglet.image.load('assets/hero/sliced/surprise.png')
-spr1 = pyglet.sprite.Sprite(img, x = 350, y = 85,)
-
-pic= pyglet.image.load('assets/hero/sliced/idle-4.png')
-spr2 = pyglet.sprite.Sprite(img, x = 150, y = 85,)
-
-spr1.scale = 10
+spr2 = pyglet.sprite.Sprite(img, x = 150, y = 20,)
 spr2.scale = 10
+
 
 # Get the key state handler object
 keys = pyglet.window.key.KeyStateHandler()
@@ -36,7 +37,6 @@ def on_draw():
     labelthree.draw()
     spr1.draw()
     spr2.draw()
-    
 
 pyglet.clock.schedule(update)
 pyglet.app.run()
